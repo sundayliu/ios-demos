439db012cf0827c898b2ff9f4978f51ea6dfcb49
chameleon <chameleonliu@gmail.com>
2014-09-27 15:42:03 +0800

    finish control fun

--- Control Fun/Control Fun/ViewController.h
+++ Control Fun/Control Fun/ViewController.h
@@ -8,6 +8,19 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UIActionSheetDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *numberField;
+@property (weak, nonatomic) IBOutlet UILabel *sliderLabel;
+@property (weak, nonatomic) IBOutlet UISwitch *leftSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *rightSwitch;
+@property (weak, nonatomic) IBOutlet UIButton *doSomethingButton;
+
+- (IBAction)textFieldDoneEditing:(id)sender;
+- (IBAction)backgroundTap:(UIControl *)sender;
+- (IBAction)sliderChanged:(UISlider *)sender;
+- (IBAction)switchChanged:(UISwitch *)sender;
+- (IBAction)toggleControls:(UISegmentedControl *)sender;
+- (IBAction)buttonPressed:(UIButton *)sender;
 
 @end
