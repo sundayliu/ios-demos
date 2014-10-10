da29bec966c6e67ce0f018dcc1a6c07d50dc8ec9
chameleon <chameleonliu@gmail.com>
2014-10-10 20:57:01 +0800

    ASIHttpRequestDemo - ASIHttpRequest Demo

---
+++ ASIHttpRequestDemo/ASIHttpRequestDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ASIHttpRequestDemo
+//
+//  Created by JUN LIU on 10/10/14.
+//  Copyright (c) 2014 JUN LIU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ ASIHttpRequestDemo/ASIHttpRequestDemoTests/ASIHttpRequestDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  ASIHttpRequestDemoTests.m
+//  ASIHttpRequestDemoTests
+//
+//  Created by JUN LIU on 10/10/14.
+//  Copyright (c) 2014 JUN LIU. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ASIHttpRequestDemoTests : XCTestCase
+
+@end
+
+@implementation ASIHttpRequestDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
